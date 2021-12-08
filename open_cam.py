936afa744e50c6e8335be549307f0d842a4deb01
mycroft_19 <jhashikhar1996@gmail.com>
2021-12-08 15:15:09 +0530

    open camera openCV

---
+++ open_cam.py
@@ -0,0 +1,20 @@
+import cv2
+# cam = cv2.VideoCapture(0)
+# while cam.isOpened():
+#     ret , frame = cam.read()
+#     # ret , frame2 = cam.read()
+#     #diff =
+#     if cv2.waitKey(10) == ord('q'):
+#         break
+#     cv2.imshow("camera" , frame)
+
+#secondway
+
+cap = cv2.VideoCapture(0)
+# cap.set()
+
+while True:
+    frame , img = cap.read()
+    if cv2.waitKey(10) & 0xFF ==ord('q'):
+        break
+    cv2.imshow("camera" , img )
