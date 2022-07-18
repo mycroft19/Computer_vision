d0eb5f8da8ef5906a296d4429d1282e0d5bc262e
mycroft19 <44457428+mycroft19@users.noreply.github.com>
2022-07-18 17:38:11 +0530

    Update open_cam.py

--- open_cam.py
+++ open_cam.py
@@ -1,19 +1,19 @@
 ##Following are the two methods which can be used to access the webcam connected to your syatem.
 
 import cv2
-# cam = cv2.VideoCapture(0)
-# while cam.isOpened():
-#     ret , frame = cam.read()
-#     # ret , frame2 = cam.read()
-#     #diff =
-#     if cv2.waitKey(10) == ord('q'):
-#         break
-#     cv2.imshow("camera" , frame)
+cam = cv2.VideoCapture(0)
+while cam.isOpened():
+    ret , frame = cam.read()
+    # ret , frame2 = cam.read()
+    #diff =
+    if cv2.waitKey(10) == ord('q'):
+        break
+    cv2.imshow("camera" , frame)
 
 #secondway
 
 cap = cv2.VideoCapture(0)
-# cap.set()
+cap.set()
 
 while True:
     frame , img = cap.read()
